8f4938b7e2c2e38ee18dd10e6b3a3205eac59279
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-10 18:52:23 -0300

    depois da aula foi feito isso

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
-const currencyAPI = (currency) => fetch(`https://api.exchangerate.host/latest?base=${currency}`)
-.then((resp) => resp.json())
-.then((data) => console.log(data.rates))
+const searchButton = document.querySelector('.search-btn');
 
-currencyAPI('USD');
+const coinInput = document.querySelector('#coin-input');
+
+const coinsList = document.querySelector('.coins');
+
+function fetchAPI(coin){
+    const url = `https://api.exchangerate.host/latest?base=${coin}`;
+    return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.rates)
+}
+
+function renderCoins(coins){
+    // deixo a lista vazia
+    coinsList.innerHTML = ''; 
+
+    // Aqui eu pego meu objeto e transformo em um array que possui vários arrays que contém a moeda de cotação e o objeto
+    const coinsArray = Object.entries(coins)
+    console.log(coinsArray);
+
+    coinsArray.forEach((coin) => {
+        const [coinName, value] = coin;
+        
+        const li = document.createElement('li');
+        li.textContent = `${coinName} - ${value}`;
+        coinsList.appendChild(li);
+    })
+}
+
+function handleSearch(){
+    const coin = coinInput.value;
+
+    fetchAPI(coin)
+    .then(renderCoins)
+}
+
+searchButton.addEventListener('click', handleSearch);
