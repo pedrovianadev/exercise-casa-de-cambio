5c2275d1b5e719d440d4ccc83947c91645bcd35f
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-10 17:44:06 -0300

    mudanças

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const currencyAPI = (currency) => fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+.then((resp) => resp.json())
+.then((data) => console.log(data.rates))
+
+currencyAPI('USD');
